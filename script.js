ba64bbf4db3d200e721185ccfc2ef1aa79d776fc
Vishal Wani <93905384+Vishalwani123@users.noreply.github.com>
2025-08-26 19:31:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstWord(s) {
   // your code here
+	s = s.trim();
+
+	if(s === "") return "";
+
+	const spaceIdx = s.indexOf(" ");
+
+	if(spaceIdx === -1){
+		return s;
+	}
+
+	return s.substring(0, spaceIdx);
 }
 
 // Do not change the code below
